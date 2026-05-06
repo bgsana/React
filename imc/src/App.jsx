f19d7423fdd23a5dc8543be219d05616c02b042d
bgsana <bgs.ana08@gmail.com>
2026-05-06 20:31:20 -0300

    Finalizado a calculadora de IMC

--- imc/src/App.jsx
+++ imc/src/App.jsx
@@ -3,6 +3,7 @@ import ImcCalc from './components/ImcCalc'
 import Button from './components/Button'
 import { useState } from 'react';
 import { data } from "./data/data";
+import ImcTable from './components/ImcTable';
 
 function App() {
   const calcImc = (e, height, weight) => {
@@ -20,14 +21,14 @@ function App() {
 
     // Percorre os dados para encontrar a classifição correta
     data.forEach((item) => {
-      if (imcResult >= item.min && imcResult <= item.max){
+      if (imcResult >= item.min && imcResult <= item.max) {
         setInfo(item.info);
         setInfoClass(item.infoClass);
       }
     });
   }
-  
-  const resetCalc = () =>{
+
+  const resetCalc = () => {
     setImc("");
     setInfo("");
     setInfoClass("");
@@ -40,7 +41,16 @@ function App() {
 
   return (
     <div className="container">
-      <ImcCalc calcImc={calcImc} />
+      {/* Renderização Condicional
+          - Se não tem IMC calculadp -> mostra formulário (ImcCalc)
+          - Se tem IMC calculado -> mostra resultado (ImcTable)
+      */}
+      {!imc ? (
+        <ImcCalc calcImc={calcImc} />
+      ) : (
+        <ImcTable data={data} imc={imc} info={info} infoClass={infoClass} resetCalc={resetCalc} />
+      )
+      }
     </div>
   )
 }
